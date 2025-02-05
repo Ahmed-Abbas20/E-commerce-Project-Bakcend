8658fa335432fc1c9852883b66f0d77ca301edbe
Mohamed Elmahdy <engmohamedelmahdy935@gmail.com>
2025-02-05 13:15:52 +0200

    ----

--- models/inventory.model.js
+++ models/inventory.model.js
@@ -1,6 +1,6 @@
 const InventorySchema = new mongoose.Schema({
     productId: { type: String, required: true},
-      providerID: {type: String , required: true},
+    providerID: {type: String , required: true},
     providerName: { type: String, required: true },
     productName: { type: String, required: true },
     currentStock: { type: Number, required: true },
