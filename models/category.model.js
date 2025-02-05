44a76cb11c137b1164e347983b4f416d03361fd1
Mohamed Elmahdy <engmohamedelmahdy935@gmail.com>
2025-02-05 13:15:52 +0200

    ----

--- models/category.model.js
+++ models/category.model.js
@@ -16,20 +16,20 @@ trim: true
 
 },)
 
-//delete products related to category
-// CategorySchema.pre('remove', async function(next) {
-//   await mongoose.model('Product').deleteMany({ categoryId: this._id });
-//   next();
-// });
+// delete products related to category
+CategorySchema.pre('remove', async function(next) {
+  await mongoose.model('Product').deleteMany({ categoryId: this._id });
+  next();
+});
 
-// // Update product category names
-// CategorySchema.post('findOneAndUpdate', async function(doc) {
-//   if (doc && doc.isModified('name')) {
-//     await mongoose.model('Product').updateMany(
-//       { categoryId: doc._id },
-//       { $set: { categoryName: doc.name } }
-//     );
-//   }
-// });
+// Update product category names
+CategorySchema.post('findOneAndUpdate', async function(doc) {
+  if (doc && doc.isModified('name')) {
+    await mongoose.model('Product').updateMany(
+      { categoryId: doc._id },
+      { $set: { categoryName: doc.name } }
+    );
+  }
+});
 
 module.exports = mongoose.model('Category', CategorySchema);
