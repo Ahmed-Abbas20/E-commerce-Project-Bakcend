8ccdc9e5b9a8f9c050a5955b3e68ac9ca063f3ed
Abdulrahman Abuelgheit <abdulrahmanabuelgheit5@gmail.com>
2025-02-15 21:33:28 +0200

    v3

--- models/product.model.js
+++ models/product.model.js
@@ -1,51 +1,16 @@
 
-
 const mongoose = require('mongoose');
 const ProductSchema = new mongoose.Schema({
-
-  name: {
-    type: String,
-    required: true,
-    trim: true,
-    unique: true
-  },
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  images: [
-    {
-      fileId: { type: String, required: true }, 
-      filePath: { type: String, required: true } 
-    }
-  ],
+  name: { type: String, required: true, trim: true, unique: true },
+  price: { type: Number, required: true, min: 0 },
+  images: [{ fileId: { type: String, required: true }, filePath: { type: String, required: true } }],
   description: String,
-  quantity: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-  categoryId: {
-    type: String,
-    ref: 'Category',
-    required: true
-  },
-  categoryName: {
-    type: String,
-    required: true
-  },
-
-
-  sellerId:{type: String, ref:'User'},
-
-  createdAt: {
-    type: Date,
-    default: Date.now()
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now()
-  }
+  mainStock: { type: Number, min: 0 }, // Central stock for all branches
+  categoryId: { type: mongoose.Schema.Types.ObjectId, ref: 'Category', required: true },
+  categoryName: { type: String, required: true },
+  sellerId: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now }
 });
+
 module.exports = mongoose.model('Product', ProductSchema);
